fe9dc09db3c741f434606bde1c24463365709c4f
Alien7666 <jason6080717@gmail.com>
2023-08-13 15:51:11 +0800

    後臺管理連結弄好了

--- src/main/java/dev/excelhunt/excel/ControlController.java
+++ src/main/java/dev/excelhunt/excel/ControlController.java
@@ -1,41 +1,71 @@
 package dev.excelhunt.excel;
 
+import com.google.cloud.storage.*;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import java.nio.file.*;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+import java.io.FileInputStream;
 import java.io.IOException;
-@RestController
-@RequestMapping("/api")
+
+import java.nio.charset.StandardCharsets;
+
+
 @Controller
 public class ControlController {
+
+    private static final String BUCKET_NAME = "link_file";
+    private static final String OBJECT_NAME = "link_file.txt";
+    private Storage storage;
+
+    public ControlController() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/excel-cloud-search-703c4ad642b3.json"));
+        storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
+
     @GetMapping("/control")
     public ModelAndView controlPage(HttpSession session) {
+        ModelAndView modelAndView;
         // 檢查session是否包含username
         if (session.getAttribute("username") != null) {
             // 如果用戶已登入，顯示管理頁面
-            return new ModelAndView("bk/bk");
+            modelAndView = new ModelAndView("bk/bk");
+
+            // Retrieve the link from GCS
+            String link = readLinkFromGCS();
+            modelAndView.addObject("link", link);
+
         } else {
             // 如果用戶未登入，重定向到登入頁面
-            return new ModelAndView("redirect:/login");
+            modelAndView = new ModelAndView("redirect:/login");
         }
-    }
-    @GetMapping("/read-link")
-    public ResponseEntity<String> readLink() {
-        try {
-            String LINK_FILE_PATH = "改成雲端連結";
-            String link = Files.readString(Paths.get(LINK_FILE_PATH));
-            return ResponseEntity.ok(link);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading link");
-        }
+        return modelAndView;
     }
 
+    private String readLinkFromGCS() {
+        Blob blob = storage.get(BlobId.of(BUCKET_NAME, OBJECT_NAME));
+        if (blob == null) {
+
+            System.out.println("Blob not found for BUCKET: " + BUCKET_NAME + " and OBJECT: " + OBJECT_NAME);
+
+            return "無法存取連結";
+        }
+        return new String(blob.getContent(), StandardCharsets.UTF_8);
+    }
+    @PostMapping("/update-link")
+    public String updateLink(@RequestParam String newLink) {
+        // Update the link in GCS
+        byte[] bytes = newLink.getBytes(StandardCharsets.UTF_8);
+        BlobId blobId = BlobId.of(BUCKET_NAME, OBJECT_NAME);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
+        storage.create(blobInfo, bytes);
+
+        return "redirect:/";
+    }
 
 
 }
